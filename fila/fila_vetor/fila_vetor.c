89020211ba12edd51f28d47a5f6fd18647690249
perasjoao <joaovictor.bsb2810@gmail.com>
2026-09-20 14:07:59 -0300

    fila com vetor

--- fila/fila_vetor/fila_vetor.c
+++ fila/fila_vetor/fila_vetor.c
@@ -2,13 +2,20 @@
 
 fila*cria_fila(int capacidade){
     fila*f=calloc(1,sizeof(fila));
-    f->v=calloc(capacidade,sizeof(int);
+    f->v=calloc(capacidade,sizeof(int));
     f->capacidade=capacidade;
     return f;
 }
 
 void enfileira(fila*f,int val){
-    
+    f->v[f->fim]=val;
+    f->fim=(f->fim+1)%f->capacidade;
+    f->tamanho++;
 }
 
-int desenfileira(fila*f,int val);
+int desenfileira(fila*f,int val){
+    int save=f->v[f->inicio];
+    f->inicio=(f->inicio+1)%f->capacidade;
+    f->tamanho--;
+    return save;
+}
